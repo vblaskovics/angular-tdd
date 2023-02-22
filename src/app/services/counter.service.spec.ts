069f4e017a797cbae5ccaee9ec1854b007d03602
vblaskovics <viktor.blaskovics@gmail.com>
2023-02-22 16:12:03 +0100

    Add rxmarble unit test

--- src/app/services/counter.service.spec.ts
+++ src/app/services/counter.service.spec.ts
@@ -1,6 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CounterService } from './counter.service';
+import { TestScheduler } from 'rxjs/testing';
+import { ReplaySubject } from 'rxjs';
+
+const testScheduler = new TestScheduler((actual, expected) => {
+  expect(actual).toEqual(expected);
+});
 
 describe('CounterService', () => {
   let service: CounterService;
@@ -16,19 +22,41 @@ describe('CounterService', () => {
 
   it('should provide a counter stream', () => {
     expect(service.getCounter$()).toBeDefined();
-  })
+  });
 
   xit('should be able to increment a counter value', () => {
-    let counter$ = service.getCounter$()
-    // expect(counter$.getValue()).toBe(0);
-    // service.incrementCounter();
-    // expect(counter$.getValue()).toBe(1);
+    let counter$ = service.getCounter$();
+    let callCount = 0;
 
     counter$.subscribe((value) => {
-      expect(value).toBe(0);
+      switch (callCount) {
+        case 0:
+          expect(value).toBe(0);
+          break;
+        case 1:
+          expect(value).toBe(1);
+          break;
+        default:
+          fail('Unexpected call count');
+      }
+      callCount++;
     });
     service.incrementCounter();
-  })
+  });
+
+  it('should be able to increment a counter value', () => {
+    let counter$ = service.getCounter$();
+    let replaySubject$ = new ReplaySubject<number>();
+    counter$.subscribe(replaySubject$);
+    service.incrementCounter();
+    service.incrementCounter();
+    service.incrementCounter();
+
+    testScheduler.run((helpers) => {
+      const { expectObservable } = helpers;
+      expectObservable(replaySubject$).toBe('(abcd)', { a: 0, b: 10, c: 2, d: 3 });
+    });
+  });
 
   // it('should be able ro decrease a counter value', () => {
   //   let counter$ = service.getCounter$()
